e6c6b99331bd09c7272c45933de4947de90e220d
rick-sys <ricksanchezy2x@gmail.com>
2024-05-20 05:58:12 +0200

    rr

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -1,5 +1,5 @@
 import { useUIState } from 'ai/rsc'
-import type { AI } from '@/app/action'
+import type { AI } from '@/app/actions'
 
 export function ChatMessages() {
   const [messages, setMessages] = useUIState<typeof AI>()
